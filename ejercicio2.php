d39aee5cef45f126f00e7455e5ae56d350bdf09b
Oscar Castro <alexcaxtro@gmail.com>
2023-06-12 22:12:37 -0400

    primer commit

---
+++ ejercicio2.php
@@ -0,0 +1,28 @@
+<?php
+
+    //recibir información del formulario html
+    if($_POST){
+        $nombre=$_POST['txtNombre'];
+        echo "Hola ".$nombre;
+
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ejercicio2.php" method="post">
+    <input type="text" name="txtNombre" id="">
+    <br/>
+    <input type="submit" value="Enviar">
+
+    </form>
+</body>
+</html>
